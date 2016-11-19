0050e136792d37d923338dffb6a85670fe0aaae8
lthr <mikelothar@gmail.com>
2016-11-19 15:17:22 +0100

    fix

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # X
+
+## Token
+
+1. Generate token from GitHub
+2. travis encrypt -r lthr/X GITHUB_TOKEN=insert-token-key-here --add
--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -9,7 +9,10 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
-harp compile . $TEMP_DIRECTORY || exit 1
+
+echo <h2>XXX</h2> > $TEMP_DIRECTORY/index.html || exit 1
+
+# harp compile . $TEMP_DIRECTORY || exit 1
 cp .gitignore $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
@@ -23,13 +26,13 @@ cp -r $TEMP_DIRECTORY/* . || exit 1
 cp $TEMP_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
-git config user.name "Travis-CI" || exit 1
-git config user.email "travis@jvandemo.com" || exit 1
+git config user.name "lthr" || exit 1
+git config user.email "mikelothar@gmail.com" || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-# git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
+git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+# git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
 
 echo "Cleaning up temp files"
 rm -Rf $TEMP_DIRECTORY
