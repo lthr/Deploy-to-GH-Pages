8fd54014908975d6302f27cf95d4051176963640
lthr <mikelothar@gmail.com>
2016-11-19 15:42:29 +0100

    cleanup

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -2,19 +2,19 @@
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
-TEMP_DIRECTORY="/tmp/__temp_static_content"
+DIST_DIRECTORY="/dist"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
 echo "Compiling new static content"
-mkdir $TEMP_DIRECTORY || exit 1
+mkdir $DIST_DIRECTORY || exit 1
 
 # fake content
-echo XXX > $TEMP_DIRECTORY/index.html || exit 1
+echo XXX > $DIST_DIRECTORY/index.html || exit 1
 
 # harp compile . $TEMP_DIRECTORY || exit 1
-cp .gitignore $TEMP_DIRECTORY || exit 1
+# cp .gitignore $DIST_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
@@ -23,8 +23,8 @@ echo "Removing old static content"
 git rm -rf . || exit 1
 
 echo "Copying newly generated static content"
-cp -r $TEMP_DIRECTORY/* . || exit 1
-cp $TEMP_DIRECTORY/.gitignore . || exit 1
+cp -r $DIST_DIRECTORY/* . || exit 1
+# cp $DIST_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "lthr" || exit 1
@@ -36,7 +36,7 @@ git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&
 # git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages
 
 echo "Cleaning up temp files"
-rm -Rf $TEMP_DIRECTORY
+rm -Rf $DIST_DIRECTORY
 
 echo "Deployed successfully."
 exit 0
