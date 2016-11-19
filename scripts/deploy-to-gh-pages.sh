98ff893d07e413af4a8995ec51d2d7bb45131437
lthr <mikelothar@gmail.com>
2016-11-19 14:09:47 +0100

    fix

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -9,5 +9,6 @@ mkdir out;
  cp ../hello.js ./hello.js
  git add .
  git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+ # git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+ git push --force "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages
 )
