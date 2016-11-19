f94cc6cea61aab02402df6ce6fdf104adabf673d
lthr <mikelothar@gmail.com>
2016-11-19 16:07:20 +0100

    better scripting

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -7,14 +7,7 @@ CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo "Compiling new static content"
-# mkdir $DIST_DIRECTORY || exit 1
-
-# fake content
-# echo XXX > $DIST_DIRECTORY/index.html || exit 1
-
-# harp compile . $TEMP_DIRECTORY || exit 1
-cp .gitignore $DIST_DIRECTORY || exit 1
+# cp .gitignore $DIST_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
@@ -22,9 +15,9 @@ git checkout -B gh-pages || exit 1
 echo "Removing old static content"
 git rm -rf . || exit 1
 
-echo "Copying newly generated static content"
+echo "Copying dist content to root"
 cp -r $DIST_DIRECTORY/* . || exit 1
-cp $DIST_DIRECTORY/.gitignore . || exit 1
+# cp $DIST_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "lthr" || exit 1
