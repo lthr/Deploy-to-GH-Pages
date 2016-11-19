18f16d395736cfbf341f17f437b141326f079197
lthr <mikelothar@gmail.com>
2016-11-19 15:24:58 +0100

    fix

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -10,7 +10,8 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
 
-echo <h2>XXX</h2> > $TEMP_DIRECTORY/index.html || exit 1
+# fake content
+# echo XXX > $TEMP_DIRECTORY/index.html || exit 1
 
 # harp compile . $TEMP_DIRECTORY || exit 1
 cp .gitignore $TEMP_DIRECTORY || exit 1
