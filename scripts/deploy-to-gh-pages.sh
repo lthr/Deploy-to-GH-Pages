989724cba998858a042f7ac122652669a1217b82
Travis CI <nobody@nobody.org>
2016-11-19 14:04:17 +0100

    fix

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -4,10 +4,10 @@ mkdir out;
 #node build.js
 ( cd out
  git init
- git config user.name "Travis-CI"
- git config user.email "travis@gkdhfgkfdhgkdfgkhdfghdkfhgkjdfhgkdfhgjdh.com"
+ git config user.name "lthr"
+ git config user.email "mikelothar@gmail.com"
  cp ../hello.js ./hello.js
  git add .
  git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:ghpages > /dev/null 2>&1
+ git push --force --quiet "https://${GITHUB_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
