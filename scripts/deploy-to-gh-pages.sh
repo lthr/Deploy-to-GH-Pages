2ab80bbe1d006425a61a793d3b702aaaba38cc1f
lthr <mikelothar@gmail.com>
2016-11-19 14:23:24 +0100

    fix

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -28,7 +28,8 @@ git config user.email "travis@jvandemo.com" || exit 1
 
 git add -A . || exit 1
 git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+# git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+git push --force "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages2
 
 echo "Cleaning up temp files"
 rm -Rf $TEMP_DIRECTORY
